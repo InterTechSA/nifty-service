6539d8fe3aa0f54a9caf970e47da96df3769427b
Thabang Kubheka <thabang.kubheka@vcontractor.co.za>
2024-06-17 14:32:53 +0200

    Refactor

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const https = require('https');
+const fs = require('fs');
 const routes = require('./api/routes/user_route');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use("/", routes);
 
+const options = {
+  key: fs.readFileSync('./privatekey.pem'),     
+  cert: fs.readFileSync('./server.crt')  
+};
+
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+https.createServer(options, app).listen(PORT, () => {
+  console.log(`Server is running on HTTPS port ${PORT}`);
 });
